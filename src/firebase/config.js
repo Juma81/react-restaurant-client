4dd50aedf134d29d98ebfe683752d2ee90e70cee
jon0026 <80295159+jon0026@users.noreply.github.com>
2022-05-30 18:54:26 -0600

    restaurantClient

---
+++ src/firebase/config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCjT-s-lgkafpG1pX2XEc2_Lmr3LEBAZKI",
+    authDomain: "restaurant-app-d233e.firebaseapp.com",
+    projectId: "restaurant-app-d233e",
+    storageBucket: "restaurant-app-d233e.appspot.com",
+    messagingSenderId: "461661178133",
+    appId: "1:461661178133:web:95256a8efa9ea606ac811c",
+    measurementId: "G-T7SYZ9DKP7"
+  };
+
+  export default firebaseConfig;
